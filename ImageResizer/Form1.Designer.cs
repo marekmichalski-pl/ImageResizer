7279aca03395170ae2e58a31c54a5a839c52435d
Marek Michalski <marek_michalski@o2.pl>
2019-04-19 21:25:44 +0200

    Select input folder.

--- ImageResizer/Form1.Designer.cs
+++ ImageResizer/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbxInputFolder = new System.Windows.Forms.GroupBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.lblSelectedInputFolder = new System.Windows.Forms.Label();
+            this.gbxInputFolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxInputFolder
+            // 
+            this.gbxInputFolder.Controls.Add(this.lblSelectedInputFolder);
+            this.gbxInputFolder.Controls.Add(this.btnSelectFolder);
+            this.gbxInputFolder.Location = new System.Drawing.Point(12, 12);
+            this.gbxInputFolder.Name = "gbxInputFolder";
+            this.gbxInputFolder.Size = new System.Drawing.Size(776, 128);
+            this.gbxInputFolder.TabIndex = 0;
+            this.gbxInputFolder.TabStop = false;
+            this.gbxInputFolder.Text = "Input folder:";
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(17, 34);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(165, 35);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select input folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // lblSelectedInputFolder
+            // 
+            this.lblSelectedInputFolder.AutoSize = true;
+            this.lblSelectedInputFolder.Location = new System.Drawing.Point(14, 91);
+            this.lblSelectedInputFolder.Name = "lblSelectedInputFolder";
+            this.lblSelectedInputFolder.Size = new System.Drawing.Size(148, 17);
+            this.lblSelectedInputFolder.TabIndex = 1;
+            this.lblSelectedInputFolder.Text = "lblSelectedInputFolder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbxInputFolder);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbxInputFolder.ResumeLayout(false);
+            this.gbxInputFolder.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbxInputFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label lblSelectedInputFolder;
     }
 }
 
--- ImageResizer/Form1.cs
+++ ImageResizer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,37 @@ namespace ImageResizer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            lblSelectedInputFolder.Text = SelectFolder();
+            CountFilesInFolder(lblSelectedInputFolder.Text, "*.jpg");
+        }
+
+
+        private string SelectFolder()
+        {
+            string path = "";
+
+            using (var fldrDlg = new FolderBrowserDialog())
+            {
+                if (fldrDlg.ShowDialog() == DialogResult.OK)
+                {
+                    path = fldrDlg.SelectedPath;
+                }
+            }
+
+            return path;
+        }
+
+        private int CountFilesInFolder(string path, string fileTye)
+        {
+            int count = 0;
+
+            count = Directory.GetFiles(path, fileTye, SearchOption.TopDirectoryOnly).Length;
+
+            return count;
         }
     }
 }
