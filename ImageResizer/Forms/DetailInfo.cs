3ad0afd5ea9cf87ca239656a0febeefeb29b59e7
Marek Michalski <marek_michalski@o2.pl>
2019-05-01 22:27:16 +0200

    Add detail info form.

---
+++ ImageResizer/Forms/DetailInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageResizer.Forms
+{
+    public partial class ImageResizer : Form
+    {
+        public ImageResizer(string fileParth)
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = new Bitmap("fileParth");
+        }
+
+        
+    }
+}
